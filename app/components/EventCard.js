a157812d0fe6b735c28e3567bfaf6f39598e4784
MVCorreas <correasmv@gmail.com>
2026-02-01 17:14:04 -0300

    feat: added basic event card and styled

---
+++ app/components/EventCard.js
@@ -0,0 +1,13 @@
+export default function EventCard({ title, date, description }) {
+  return (
+    <div className="flex items-start gap-4 p-4 bg-white rounded-md shadow-sm">
+      <div className="min-w-27.5 text-sm font-semibold text-gray-700">
+        2026-02-02
+      </div>
+      <div className="flex-1">
+        <h3 className="text-base font-semibold text-gray-900">title</h3>
+        <p className="text-sm text-gray-600 mt-1">description</p>
+      </div>
+    </div>
+  );
+}
--- app/components/EventsContainer.js
+++ app/components/EventsContainer.js
@@ -1,7 +1,10 @@
-export default function EventsContainer () {
-    return (
-         <div className="bg-white rounded-lg shadow-lg p-8">
-            <h2 className="mb-6 text-3xl font-bold text-gray-800">Events List</h2>
-         </div>
-    )
+import EventCard from "./EventCard";
+
+export default function EventsContainer({ title, date, description }) {
+  return (
+    <div className="bg-white rounded-lg shadow-lg p-8">
+      <h2 className="mb-6 text-3xl font-bold text-gray-800">Events List</h2>
+      <EventCard title={title} date={date} description={description} />
+    </div>
+  );
 }
